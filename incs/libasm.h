a32d41a302eb1fc43b35dadf2d19fea90d42b037
florian <florian.guiramand@protonmail.com>
2026-05-12 15:25:14 +0200

    feat: update ft_strlen signature and add ft_strcpy declaration

--- incs/libasm.h
+++ incs/libasm.h
@@ -1,7 +1,7 @@
 #ifndef LIBASM_H
 # define LIBASM_H
 
-unsigned char	ft_strlen(char* caca);
-
+size_t	ft_strlen(char *str);
+char	*ft_strcpy(char * dst, const char * src);
 int		ft_strcmp(const char *s1, const char *s2);
 #endif
